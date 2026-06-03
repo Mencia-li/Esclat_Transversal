f22ae55e26cc64ff95d5952b7f021cd68bb49e7c
Mencia-li <mcaljy@gmail.com>
2026-06-03 18:21:09 +0200

    Página web terminada v1.1 (a falta de algunos recursos e imágenes)
    Mejora en los planos

--- src/pages/02_program/festival/festivalMaps.ts
+++ src/pages/02_program/festival/festivalMaps.ts
@@ -47,7 +47,7 @@ const levelOneZones: FestivalMapZone[] = [
       "El espacio principal del festival. Aquí tienen lugar las charlas y conversaciones de cada día, con capacidad para 80 personas y formato abierto al debate.",
     shapes: [
       {
-        d: "M2288 633L3329 636L3329 1949L2988 1952L2982 1512L2722 1512L2722 1776L2288 1776Z",
+        d: "M2110 633L2546 636L2546 809L2811 812L2808 633L3151 633L3152 1947L2811 1947L2808 1770L2810 1509L2548 1512L2548 1771L2109 1768Z",
       },
     ],
   },
@@ -58,7 +58,7 @@ const levelOneZones: FestivalMapZone[] = [
     location: "Hall La Polivalent",
     description:
       "Una instalación permanente los tres días: entras solo, escuchas cinco minutos y dejas una palabra en el mural. El mural crece durante todo el festival.",
-    shapes: [{ d: "M2285 1776L2285 2036L2722 2036L2722 2281L2982 2278L2982 1770Z" }],
+    shapes: [{ d: "M2113 1771L2811 1774L2811 2280L2549 2280L2546 2036L2110 2033Z" }],
   },
   {
     id: "03",
@@ -82,7 +82,7 @@ const levelOneZones: FestivalMapZone[] = [
     activity: "Fotografías y álbumes",
     location: "Nave 1, planta baja",
     description: exposicionesDescription,
-    shapes: [{ d: "M1318 637L1318 1160L1585 1164L1585 1777L1932 1774L1929 635Z" }],
+    shapes: [{ d: "M1144 638L1144 1162L1412 1165L1412 1775L1756 1769L1756 632Z" }],
   },
   {
     id: "06",
@@ -91,7 +91,7 @@ const levelOneZones: FestivalMapZone[] = [
     location: "Patio 1 y Patio 2",
     description:
       "Los talleres de instrumento en rotación continua ocurren aquí: batería, djembé, ukelele, flauta, percusión latina y teclado. El domingo acoge también la batucada de cierre.",
-    shapes: [{ d: "M1935 199H2285V2283H1935Z" }],
+    shapes: [{ d: "M1756 197H2110V2279H1756Z" }],
   },
   {
     id: "07",
@@ -100,7 +100,7 @@ const levelOneZones: FestivalMapZone[] = [
     location: "Patio 1 y Patio 2",
     description:
       "Los talleres de instrumento en rotación continua ocurren aquí: batería, djembé, ukelele, flauta, percusión latina y teclado. El domingo acoge también la batucada de cierre.",
-    shapes: [{ d: "M180 197H871V2279H180Z" }],
+    shapes: [{ d: "M7 198H702V2281H7Z" }],
   },
   {
     id: "08",
@@ -109,7 +109,7 @@ const levelOneZones: FestivalMapZone[] = [
     location: "Nave 1, planta baja",
     description:
       "Dos proyecciones por día - documentales y ficción - con debate opcional al terminar. Aforo limitado a 40 personas.",
-    shapes: [{ d: "M1493 1946H1930V2281H1493Z" }],
+    shapes: [{ d: "M1318 1948H1757V2283H1318Z" }],
   },
   {
     id: "09",
@@ -117,7 +117,7 @@ const levelOneZones: FestivalMapZone[] = [
     activity: "Entrevistas",
     location: "Nave 1, planta baja",
     description: "Espacio para podcasts y entrevistas.",
-    shapes: [{ d: "M2288 2037H2722V2279H2288Z" }],
+    shapes: [{ d: "M2112 2037H2548V2281H2112Z" }],
   },
   {
     id: "INFO",
@@ -125,7 +125,7 @@ const levelOneZones: FestivalMapZone[] = [
     activity: "Ventanilla",
     location: "Nave 2, planta baja",
     description: "Punto de información en ventanilla para resolver dudas del festival.",
-    shapes: [{ d: "M875 1944H1231V2282H875Z" }],
+    shapes: [{ d: "M703 1946H1059V2281H703Z" }],
   },
   {
     id: "CAFÉ",
@@ -133,7 +133,7 @@ const levelOneZones: FestivalMapZone[] = [
     activity: "Cafetería",
     location: "Nave 2, planta baja",
     description: "Zona de cafetería.",
-    shapes: [{ d: "M878 632H1231V1775H878Z" }],
+    shapes: [{ d: "M702 634H1059V1773H702Z" }],
   },
   {
     id: "WC",
@@ -142,8 +142,9 @@ const levelOneZones: FestivalMapZone[] = [
     location: "Naves 1 y 2, planta baja",
     description: servicesDescription,
     shapes: [
-      { d: "M1317 1163H1580V1509H1317Z" },
-      { d: "M2987 1951H3325V2283H2987Z" },
+      { d: "M1141 1159H1409V1510H1141Z" },
+      { d: "M2816 1949H3155V2282H2816Z" },
+      { d: "M2547 636H2809V807H2547Z" },
     ],
   },
   {
@@ -153,10 +154,10 @@ const levelOneZones: FestivalMapZone[] = [
     location: "Naves 1 y 2, planta baja",
     description: stairsDescription,
     shapes: [
-      { d: "M1318 1512H1585V1773H1318Z" },
-      { d: "M2722 1514H2982V1768H2722Z" },
-      { d: "M1316 197H1583V464H1316Z" },
-      { d: "M2722 198H2982V458H2722Z" },
+      { d: "M1142 1509H1410V1771H1142Z" },
+      { d: "M1143 197H1408V462H1143Z" },
+      { d: "M2548 197H2810V462H2548Z" },
+      { d: "M2548 1513H2807V1769H2548Z" },
     ],
   },
 ]
@@ -169,7 +170,7 @@ const levelTwoZones: FestivalMapZone[] = [
     activity: "Talleres",
     location: "Nave 1, planta superior",
     description: factoriaDescription,
-    shapes: [{ d: "M1041 517H1198V798H1041Z" }],
+    shapes: [{ d: "M1386 1134H1734V1754H1386Z" }],
   },
   {
     id: "04",
@@ -177,7 +178,7 @@ const levelTwoZones: FestivalMapZone[] = [
     activity: "Talleres",
     location: "Nave 1, planta superior",
     description: visualRoomDescription,
-    shapes: [{ d: "M1041 281H1198V516H1041Z" }],
+    shapes: [{ d: "M1387 616H1733V1132H1387Z" }],
   },
   {
     id: "WC",
@@ -186,8 +187,8 @@ const levelTwoZones: FestivalMapZone[] = [
     location: "Naves 1 y 2, planta superior",
     description: servicesDescription,
     shapes: [
-      { d: "M604 517H722V678H604Z" },
-      { d: "M1244 281H1362V475H1244Z" },
+      { d: "M428 1135H691V1489H428Z" },
+      { d: "M1834 616H2093V1046H1834Z" },
     ],
   },
   {
@@ -197,9 +198,8 @@ const levelTwoZones: FestivalMapZone[] = [
     location: "Naves 1 y 2, planta superior",
     description: stairsDescription,
     shapes: [
-      { d: "M604 679H721V798H604Z" },
-      { d: "M1245 680H1363V799H1245Z" },
-      { d: "M1241 85H1362V198H1241Z" },
+      { d: "M426 1489H690V1755H426Z" },
+      { d: "M1833 1492H2094V1755H1833Z" },
     ],
   },
 ]
@@ -213,7 +213,11 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 1, planta baja",
     description:
       "El espacio principal del festival. Aquí tienen lugar las charlas y conversaciones de cada día, con capacidad para 80 personas y formato abierto al debate.",
-    shapes: [{ d: "M280 787A20 20 0 1 0 240 787A20 20 0 1 0 280 787Z" }],
+    shapes: [
+      {
+        d: "M4 1779L249 1775L253 1904L555 1907L555 1779L687 1779L687 1904L982 1907L978 1779L1227 1779L1227 2057L1014 2057L1014 2270L914 2270L914 2057L701 2057L701 2270L477 2270L477 2103L256 2103L256 2035L4 2039Z",
+      },
+    ],
   },
   {
     id: "02",
@@ -223,8 +227,8 @@ const naveThreeZones: FestivalMapZone[] = [
     description:
       "Una instalación permanente los tres días: entras solo, escuchas cinco minutos y dejas una palabra en el mural. El mural crece durante todo el festival.",
     shapes: [
-      { d: "M309 802H392V886H309Z" },
-      { d: "M431 803H513V886H431Z" },
+      { d: "M700 2058H913V2271H700Z" },
+      { d: "M1017 2060H1231V2274H1017Z" },
     ],
   },
   {
@@ -234,8 +238,8 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 1, planta superior",
     description: factoriaDescription,
     shapes: [
-      { d: "M99 300H253V622H99Z" },
-      { d: "M303 301H454V621H303Z" },
+      { d: "M159 766H557V1595H159Z" },
+      { d: "M687 769H1074V1594H687Z" },
     ],
   },
   {
@@ -245,9 +249,9 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 1, planta superior",
     description: visualRoomDescription,
     shapes: [
-      { d: "M573 303H608V612H573Z" },
-      { d: "M611 621H965V655H611Z" },
-      { d: "M965 305H1001V617H965Z" },
+      { d: "M1383 772H1475V1569H1383Z" },
+      { d: "M1475 1593H2389V1678H1475Z" },
+      { d: "M2391 779H2483V1579H2391Z" },
     ],
   },
   {
@@ -257,8 +261,8 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 1, planta baja",
     description: exposicionesDescription,
     shapes: [
-      { d: "M1084 303H1122V658H1084Z" },
-      { d: "M1476 304H1511V658H1476Z" },
+      { d: "M2697 771H2796V1689H2697Z" },
+      { d: "M3708 772H3801V1688H3708Z" },
     ],
   },
   {
@@ -268,8 +272,8 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 3",
     description: servicesDescription,
     shapes: [
-      { d: "M136 822H221V885H136Z" },
-      { d: "M1159 823H1243V888H1159Z" },
+      { d: "M256 2108H477V2275H256Z" },
+      { d: "M2887 2110H3108V2277H2887Z" },
     ],
   },
   {
@@ -279,12 +283,12 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 3",
     description: stairsDescription,
     shapes: [
-      { d: "M135 694H252V743H135Z" },
-      { d: "M303 694H416V743H303Z" },
-      { d: "M644 698H762V748H644Z" },
-      { d: "M812 697H927V748H812Z" },
-      { d: "M1153 697H1268V747H1153Z" },
-      { d: "M1324 697H1438V747H1324Z" },
+      { d: "M253 1780H551V1908H253Z" },
+      { d: "M687 1783H978V1907H687Z" },
+      { d: "M1563 1789H1871V1919H1563Z" },
+      { d: "M1999 1789H2291V1917H1999Z" },
+      { d: "M2877 1785H3168V1913H2877Z" },
+      { d: "M3317 1784H3608V1912H3317Z" },
     ],
   },
   {
@@ -294,9 +298,9 @@ const naveThreeZones: FestivalMapZone[] = [
     location: "Nave 3",
     description: liftsDescription,
     shapes: [
-      { d: "M39 796H136V885H39Z" },
-      { d: "M548 798H647V888H548Z" },
-      { d: "M1059 797H1157V888H1059Z" },
+      { d: "M5 2040H257V2275H5Z" },
+      { d: "M1317 2047H1570V2282H1317Z" },
+      { d: "M2632 2045H2885V2276H2632Z" },
     ],
   },
 ]
@@ -307,9 +311,9 @@ export const festivalMaps: FestivalMapConfig[] = [
     id: "level-one",
     image: "/img/spaces/naves-1-y2-nivel-1.png",
     alt: "Mapa de Las Naves 1 y 2, planta baja",
-    viewBox: "0 0 3508 2481",
-    width: 3508,
-    height: 2481,
+    viewBox: "0 0 3334 2290",
+    width: 3334,
+    height: 2290,
     ariaLabel: "Mapa interactivo de Las Naves 1 y 2, planta baja",
     infoOffset: "2.375rem",
     defaultZoneId: "01",
@@ -319,9 +323,9 @@ export const festivalMaps: FestivalMapConfig[] = [
     id: "level-two",
     image: "/img/spaces/naves-1-y2-nivel-2.png",
     alt: "Mapa de Las Naves 1 y 2, planta superior",
-    viewBox: "0 0 1600 1131",
-    width: 1600,
-    height: 1131,
+    viewBox: "0 0 2614 1758",
+    width: 2614,
+    height: 1758,
     ariaLabel: "Mapa interactivo de Las Naves 1 y 2, planta superior",
     infoOffset: "2.3rem",
     defaultZoneId: "03",
@@ -331,9 +335,9 @@ export const festivalMaps: FestivalMapConfig[] = [
     id: "nave-three",
     image: "/img/spaces/nave3.png",
     alt: "Mapa de Nave 3",
-    viewBox: "0 0 1600 963",
-    width: 1600,
-    height: 963,
+    viewBox: "0 0 4027 2391",
+    width: 4027,
+    height: 2391,
     ariaLabel: "Mapa interactivo de Nave 3",
     infoOffset: "2.1rem",
     defaultZoneId: "01",
